be1e62b5541e675b018217aedce7f7af9ffbeac2
Alexander Stevens <alex.stvn2005@gmail.com>
2026-02-20 07:37:51 -0800

    refact: clean up utils

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 
-//SizedBox responsiveBox(BuildContext context, double size) {
-//  double baseWidth = size;
-//  double baseHeight = size;
-//  final screenWidth = MediaQuery.of(context).size.width;
-//  final screenHeight = MediaQuery.of(context).size.height;
-//  final isDesktop = screenWidth >= 800;
-//  // If phone then swap base and height and multiply base height by aspect ratio conversion
-//  final boxWidth = isDesktop ? baseWidth : baseHeight;
-//  final boxHeight = isDesktop ? baseHeight : baseWidth * (20 / 16);
-//
-//  return SizedBox(
-//    width: (boxWidth / 200) * screenWidth,
-//    height: (boxHeight / 200) * screenHeight,
-//  );
-//}
-//
-double responsiveFont(BuildContext context, double base) {
+// For padding between items, for same look phone must pad height more (with respect to width)
+SizedBox responsiveBox(BuildContext context, double size) {
+  double baseWidth = size;
+  double baseHeight = size;
+  final screenWidth = MediaQuery.of(context).size.width;
+  final screenHeight = MediaQuery.of(context).size.height;
+  final isDesktop = screenWidth >= 800;
+  // If phone then swap base and height and multiply base height by aspect ratio conversion
+  final boxWidth = isDesktop ? baseWidth : baseHeight;
+  final boxHeight = isDesktop ? baseHeight : baseWidth * (20 / 16);
+
+  return SizedBox(
+    width: (boxWidth / 200) * screenWidth,
+    height: (boxHeight / 200) * screenHeight,
+  );
+}
+
+double responsiveFontSize(BuildContext context, double base) {
   return base * screenWidth(context) / 200;
 }
 
