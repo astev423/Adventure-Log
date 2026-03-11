db0e5a6a95997febaaa4633acbf2f8c7dac027fe
Alexander Stevens <alex.stvn2005@gmail.com>
2026-03-11 03:57:05 -0700

    feat: added fetching and displaying reviews from firestore

--- lib/views/pages/view_reviews.dart
+++ lib/views/pages/view_reviews.dart
@@ -1,3 +1,6 @@
+import 'package:adventure_log/controllers/utils/constants.dart';
+import 'package:adventure_log/data/firestore_queries.dart';
+import 'package:adventure_log/data/models/review.dart';
 import 'package:flutter/material.dart';
 
 class ViewReviews extends StatelessWidget {
@@ -6,12 +9,57 @@ class ViewReviews extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: teal,
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.pushNamed(context, '/'),
-          child: Text("Go back to home"),
+        child: Column(
+          spacing: 10,
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/'),
+              child: const Text("Go back to home"),
+            ),
+            ReviewsColumn(),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class ReviewsColumn extends StatefulWidget {
+  const ReviewsColumn({super.key});
+
+  @override
+  State<ReviewsColumn> createState() => _ReviewsColumnState();
+}
+
+class _ReviewsColumnState extends State<ReviewsColumn> {
+  List<Review> _reviews = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchReviews();
+  }
+
+  void fetchReviews() async {
+    var reviews = await fetchAllReviews();
+    setState(() {
+      if (reviews != null) {
+        _reviews = reviews;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    fetchReviews();
+    return Column(
+      children: [
+        Text("Reviews"),
+        if (_reviews.isNotEmpty) Text(_reviews[0].locationName),
+        if (_reviews.isEmpty) Text("Can't find any ratings"),
+      ],
     );
   }
 }
